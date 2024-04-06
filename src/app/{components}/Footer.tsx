258e37f9ea8153107ba59c5dc04fac450e0dc8bc
Table Computer <banvietthenaocungdc@gmail.com>
2024-04-06 19:23:39 +0700

    Align the footer text

--- src/app/{components}/Footer.tsx
+++ src/app/{components}/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="phone:max-w-[100%] phone:pt-[20px] phone:pl-[20px] phone:pr-[20px] phone:pb-[20px] mt-0 ml-auto mr-auto mb-0 text-[#888] font-[400] text-[0.875rem]">
+      <footer className="phone:max-w-[100%] phone:pt-[20px] phone:pl-[20px] phone:pr-[20px] phone:pb-[20px] desktop:mt-0 desktop:ml-auto desktop:mr-auto desktop:mb-0 text-[#888] font-[400] phone:text-[0.875rem] desktop:max-w-[45vw] desktop:font-[400] desktop:text-[0.875rem] desktop:pt-[40px] desktop:pl-[40px] desktop:pr-[40px] desktop:pb-[40px]">
         © Duy La in 2024
       </footer>
     </>
